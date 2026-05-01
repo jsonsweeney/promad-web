44965ceebba4b27156422e7ee858bcff7a6ac781
Jason Sweeney <jason.sweeney@pixeltoys.com>
2026-05-01 21:27:48 +0100

    chore: removed dupe sha256

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/.well-known/assetlinks.json",
+        headers: [{ key: "Content-Type", value: "application/json" }],
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
