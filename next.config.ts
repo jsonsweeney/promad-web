7626c039cc720494d2f37950ce775ed3cdc3e2c6
Jason Sweeney <jason.sweeney@pixeltoys.com>
2026-05-01 21:39:53 +0100

    chore: added redirects

--- next.config.ts
+++ next.config.ts
@@ -11,4 +11,13 @@ module.exports = {
       },
     ];
   },
+  async redirects() {
+    return [
+      {
+        source: "/.well-known/:file",
+        destination: "/api/.well-known/:file",
+        permanent: false,
+      },
+    ];
+  },
 };
